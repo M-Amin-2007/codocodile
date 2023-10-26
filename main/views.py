0870655d57c918ddb076449a6c623bbc81374086
Amin <mamp2007.mohammadi@gmail.com>
2023-10-26 14:21:48 +0330

    send post info api

--- main/views.py
+++ main/views.py
@@ -31,7 +31,7 @@ def create_post(request):
     """  This function create a post  """
     caption  = request.POST.get("caption")
     media_link  = request.POST.get("media_link")
-    user  = request.user
+    user  = MyUser.object.get(username=request.user.username)
     Post.objects.create(caption=caption, media_link=media_link, user=user)
     return JsonResponse({"message": "Post succesfully created"})
 
@@ -40,7 +40,22 @@ def create_post(request):
 @csrf_exempt
 def post_info(request):
     """ This function returns the post information """
-    post_id = request.POST.get("id")
+    post = request.POST.get("post")
+    mu = MyUser.objects.get(username=request.user.username)
+    if post.user is mu:
+        user_rate = None
+    else:
+        try:
+            user_rate = Rate.object.get(user=mu, post=post).rate
+        except Rate.DoesNotExist:
+            user_rate = 0
+    context = {"caption": post.caption,
+               "media_link": post.media_link,
+               "username": post.user.username,
+               "email": post.user.email,
+               "avg_rate": post.avg_rate,
+               "user_rate": user_rate}
+    return JsonResponse(context)
     
 
 @api_view(['POST'])
