1f519276d62043ed474911a67d24a32d501aa256
Amin <mamp2007.mohammadi@gmail.com>
2023-10-26 13:54:19 +0330

    edit routes

--- main/urls.py
+++ main/urls.py
@@ -5,11 +5,6 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path("post/create/", views.create_post, name="user"),
-    path("signout/", views.signout, name="signout"),
-    path("change_pass/", views.change_password, name="change_password"),
-    path("change_username/", views.change_username, name="change_username"),
-    path("change_email/", views.change_email, name="change_email"),
-    path("delete_account/", views.delete_account, name="delete_account"),
-    path("forgot_password/", views.forgot_password, name="forgot_password"),
+    path("post/create/", views.create_post, name="create_post"),
+    path("post/rate/", views.rate_post, name="rate_post"),
 ]
