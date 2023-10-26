ed3a854893ffb4ec80c624713666ebec148b7bee
Amin <mamp2007.mohammadi@gmail.com>
2023-10-26 13:29:02 +0330

    make rate route

--- main/views.py
+++ main/views.py
@@ -5,6 +5,8 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from django.contrib.auth import login, authenticate, logout
 from django.http import JsonResponse
+from users.models import MyUser
+from main.models import *
 
 
 
@@ -24,4 +26,11 @@ from django.http import JsonResponse
 
 def rate_post(request):
     """..."""
-    pass
+    rate = request.POST.get("rate")
+    post = request.POST.get("post")
+    mu = MyUser.objects.get(username=request.user.username)
+    Rate.objects.create(rate=rate, user=mu, post=post)
+    post.nor += 1
+    post.avg_rate = (post.avg_rate + rate) / post.nor
+    post.save()
+    return JsonResponse({"post": post, "new_rate": post.avg_rate})
