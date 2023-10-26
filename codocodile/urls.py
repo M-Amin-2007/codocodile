1ec5b76f17e6caa60df8dbe0718a2ebfc403b057
Amin <mamp2007.mohammadi@gmail.com>
2023-10-26 13:09:11 +0330

    connect main routes

--- codocodile/urls.py
+++ codocodile/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('users.urls'))
+    path('user/', include('users.urls')),
+    path('', include("main.urls")),
 ]
