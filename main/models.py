2f49c1383af7c2cf8d9c4cdb5e8e58a91bd69882
key <kiani.amir.88.85@gmail.com>
2023-10-26 13:31:17 +0330

    make create post route and view

--- main/models.py
+++ main/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from users import models as usermodel
 
+
 class Post(models.Model):
     caption = models.TextField()
     media_link = models.TextField()
     user = models.ForeignKey(usermodel.MyUser, on_delete=models.CASCADE)
-    nor = models.FloatField(default=0) # number of rates
+    nor = models.FloatField(default=0)  # number of rates
     avg_rate = models.FloatField(default=0)
-    
+
+
 class Rate(models.Model):
     rate = models.FloatField()
     user = models.ForeignKey(usermodel.MyUser, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    
-
--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from django.contrib.auth import login, authenticate, logout
 from django.http import JsonResponse
+from main.models import Post, Rate
 
 
 
@@ -23,5 +24,21 @@ from django.http import JsonResponse
 #--------------------------------   APIs   --------------------------------
 
 
+@api_view(['POST'])
+@csrf_exempt
+def create_post(request):
+    """  This function create a post  """
+    caption  = request.POST.get("caption")
+    media_link  = request.POST.get("media_link")
+    user  = request.user
+    Post.objects.create(caption=caption, media_link=media_link, user=user)
+    return JsonResponse({"message": "Post succesfully created"})
 
+
+@api_view(['POST'])
+@csrf_exempt
+def post_info(request):
+    """ This function returns the post information """
+    post_id = request.POST.get("id")
+    
 
