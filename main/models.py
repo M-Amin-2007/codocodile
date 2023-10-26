515a12ddac5945bda9d9fcd8cc0f556a54517a2c
key <kiani.amir.88.85@gmail.com>
2023-10-26 12:38:34 +0330

    add post and rate models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from users import models as usermodel
 
-# Create your models here.
+class Post(models.Model):
+    caption = models.TextField()
+    media_link = models.TextField()
+    user = models.ForeignKey(usermodel.MyUser, on_delete=models.CASCADE)
+    nor = models.FloatField(default=0) # number of rates
+    avg_rate = models.FloatField(default=0)
+    
+class Rate(models.Model):
+    rate = models.FloatField()
+    user = models.ForeignKey(usermodel.MyUser, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
+
