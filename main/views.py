dce34d33992b3f013e4515219a4e86c1a3319ba1
Amin <mamp2007.mohammadi@gmail.com>
2023-10-27 00:12:08 +0330

    rate posts

--- main/views.py
+++ main/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.http import JsonResponse
 from users.models import MyUser
 from main.models import *
+import json
 
 
 
@@ -25,48 +26,65 @@ from main.models import *
 #--------------------------------   APIs   --------------------------------
 
 
-@api_view(['POST'])
 @csrf_exempt
 def create_post(request):
     """  This function create a post  """
     caption  = request.POST.get("caption")
     media_link  = request.POST.get("media_link")
-    user  = MyUser.object.get(username=request.user.username)
+    user  = MyUser.objects.get(username=request.user.username)
     Post.objects.create(caption=caption, media_link=media_link, user=user)
     return JsonResponse({"message": "Post succesfully created"})
 
 
-@api_view(['POST'])
 @csrf_exempt
 def post_info(request):
     """ This function returns the post information """
-    post = request.POST.get("post")
-    mu = MyUser.objects.get(username=request.user.username)
-    if post.user is mu:
-        user_rate = None
-    else:
+    post_range = json.loads(request.POST.get("post"))
+    no_all = Post.objects.all().count()
+    context_list = list()
+    for id in range(no_all - post_range[1] + 1, no_all - post_range[0] + 2):
         try:
-            user_rate = Rate.object.get(user=mu, post=post).rate
-        except Rate.DoesNotExist:
-            user_rate = 0
-    context = {"caption": post.caption,
-               "media_link": post.media_link,
-               "username": post.user.username,
-               "email": post.user.email,
-               "avg_rate": post.avg_rate,
-               "user_rate": user_rate}
-    return JsonResponse(context)
+            post = Post.objects.get(id=id)
+        except Post.DoesNotExist:
+            break
+        mu = MyUser.objects.get(username=request.user.username)
+        if post.user is mu:
+            user_rate = post.avg_rate
+        else:
+            try:
+                user_rate = Rate.objects.get(post=post).rate
+            except Rate.DoesNotExist:
+                user_rate = 0
+        context = {"id":id,
+                "caption": post.caption,
+                "media_link": post.media_link,
+                "username": post.user.username,
+                "email": post.user.email,
+                "avg_rate": post.avg_rate,
+                "user_rate": user_rate}
+        context_list.append(context)
+    return JsonResponse({"context": json.dumps(context_list), "length": len(context_list)})
     
 
-@api_view(['POST'])
 @csrf_exempt
 def rate_post(request):
     """..."""
+    id = request.POST.get("id")
     rate = request.POST.get("rate")
-    post = request.POST.get("post")
     mu = MyUser.objects.get(username=request.user.username)
-    Rate.objects.create(rate=rate, user=mu, post=post)
-    post.nor += 1
-    post.avg_rate = (post.avg_rate + rate) / post.nor
-    post.save()
-    return JsonResponse({"post": post, "new_rate": post.avg_rate})
+    post = Post.objects.get(id=id)
+    rates = Rate.objects.filter(user=mu, post=post)
+    if rates:
+        old_rate = rates[0].rate
+        print(old_rate, post.nor, post.avg_rate)
+        post.avg_rate = (post.avg_rate * post.nor - float(old_rate) + float(rate)) / (post.nor)
+        print(post.avg_rate)
+        post.save()
+        rates[0].rate = rate
+        rates[0].save()
+    else:
+        Rate.objects.create(rate=rate, user=mu, post=post)
+        post.avg_rate = (post.avg_rate * post.nor + float(rate)) / (post.nor + 1)
+        post.nor += 1
+        post.save()
+    return JsonResponse({"post_id": id, "new_post_rate": post.avg_rate})
